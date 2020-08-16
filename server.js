dbb4125581b4e5ab75eb9a3c3614c978775c5e32
diogoluzitani <50780642+diogoluzitani@users.noreply.github.com>
2020-08-16 11:20:09 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res)=> {
-    res.status(200).send({message: 'Olá mundo estou funcionando!'});
+app.get('/ola', (req, res)=> {
+    res.status(200).send({message: 'oi eu sou a rota ola!'});
 })
 
 app.listen(3301, ()=> {
